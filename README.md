cf1a183c4e9e0821d9a051763e958c0cd45c68c5
Chr1018 <chandan18hr@gmail.com>
2022-06-08 18:47:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-laiumo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-laiumo)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="parent">
+      <div className="initailDisplay">
+        <span>CHR</span>
+      </div>
+      <div className="onHoverDisplay">
+        Hello i am Chandan HR currently in 3rd year Engineering.I am fromNIE
+        mysore. I am an intern at ExcelSoft currently.
+      </div>
+    </div>
+  );
+}
